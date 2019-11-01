62cc6a3d40023860c4f0fc9a25c194d724c5dfe5
gopikrishnapocham <gopikrishna.pocham@sharecare.com>
2019-11-01 17:43:25 +0530

    Added footer and new pages changes

--- src/pages/singin.js
+++ src/pages/singin.js
@@ -1,12 +1,47 @@
 import React from "react"
+import BuildYourTeam from "../components/home/Build-your-team"
+import Button from "../components/utils/Button"
+import ProjCycle from "../images/proj-cycle.png"
 import Layout from "../components/layout/layout"
+import Img from "gatsby-image"
+import SEO from "../components/seo"
 
-export default () => {
+
+export default props => {
+  const { data } = props
   return (
     <Layout>
-      <div style={{ color: "red" }}>
-        <h1>Welcome to sign in</h1>
+      <SEO title="Sign In" />
+      <div className="banner-set">
+        <div className="banner-title container">Sign In</div>
+        <Img fluid={props.data.services.childImageSharp.fluid} />
+      </div>
+      <div style={{ margin: "25px 150px" }}>
+        <div className="container">
+          Sign In
+        </div>
+
       </div>
     </Layout>
   )
 }
+
+export const query = graphql`
+  query {
+    projCycle: file(relativePath: { eq: "proj-cycle.png" }) {
+      childImageSharp {
+        fluid(maxWidth: 1000) {
+          ...GatsbyImageSharpFluid
+        }
+      }
+    }
+    services: file(relativePath: { eq: "services-1.png" }) {
+      childImageSharp {
+        fluid(maxWidth: 1000) {
+          ...GatsbyImageSharpFluid
+        }
+      }
+    }
+  }
+`
+
